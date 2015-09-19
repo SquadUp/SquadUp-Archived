976b456b835e25e461dda280c9d09bceb3cd7ceb
Andrew <liboan1997@gmail.com>
2015-09-19 12:12:34 -0400

    more room behavior stuff

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', function(req, res) {
 });
 
 var users = [];
-var readyCount = 0;
+
+var rooms = [];
 
 io.on("connection", function(socket) {
     console.log("User " + socket.id + " has joined.");
@@ -21,7 +22,29 @@ io.on("connection", function(socket) {
     socket.on("createRoom", function () {
     	var thisRoomId = Math.floor(Math.random() * 100000);
     	console.log("new room " + thisRoomId);
-    	socket.emit('newRoom', thisRoomId.toString());
+    	//push new room to list
+    	rooms.push({
+    		id: thisRoomId,
+    		time: 0,
+    		location: 0,
+    		details: "New room without any details"
+    	});
+
+    	socket.emit('newRoom', thisRoomId);
+    });
+
+    socket.on("joinRoom", function (id) {
+    	console.log(this.id + " Request to join room " + id);
+    	roomIndex = searchRooms(id)
+    	if (roomIndex != -1) { //so long as the room exists
+    		this.join(id);
+    		console.log(this.id + " joined room " + id);
+    		this.emit("newEventDetails", rooms[roomIndex]);
+    		console.log(rooms[roomIndex])
+    	}
+    	else {
+    		console.log("Room doesn't exist");
+    	}
     });
 
 
@@ -31,6 +54,15 @@ io.on("connection", function(socket) {
         console.log(users.length + " users connected");
     });
 });
+
+function searchRooms(id) {
+	for (var i = 0; i < rooms.length; i++) {
+		if (rooms[i].id === id) {
+			return i;
+		}
+	}
+	return -1;
+}
 
 http.listen((process.env.PORT || 7777), function() {
     console.log("Listening for connections on port 7777.");
